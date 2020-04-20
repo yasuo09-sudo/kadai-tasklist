f7f5d3fa67207b19620057902d148b4ce9fc7950
imaizumi yasuo <imaizumiy09@gmail.com>
2020-04-20 12:29:09 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-    
+    before_action :set_message, only:[:show,:edit,:update,:destroy]
   def index
         @tasks = Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
+      
   end
 
   def new
@@ -25,11 +25,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-     @task = Task.find(params[:id])
+     
   end
 
   def update
-    @task = Task.find(params[:id])
+    
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+   
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,7 +49,10 @@ class TasksController < ApplicationController
   end
   
   private
-
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
   def task_params
     params.require(:task).permit(:content)
   end
