242c0f4080f67f7fa60defe9bde4a54d0012b1e8
imaizumi yasuo <imaizumiy09@gmail.com>
2020-05-13 13:35:49 +0000

    課題完成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
     before_action :set_task, only:[:show,:edit,:update,:destroy]
+    before_action :correct_user, only: [:show,:edit,:update,:destroy]
   def index
         @tasks = Task.all
   end
@@ -14,11 +15,14 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
+    # @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to root_url　  # @taskからroot_urlに変更
+      redirect_to root_url
+      # @taskからroot_urlに変更　全角スペースでも後ろに入っていた？消したら
+      # 進んだ。別のところで先に経験していてよかった。
     
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
@@ -62,6 +66,13 @@ class TasksController < ApplicationController
   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
+    end
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
     before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
+    @users = User.order(id: :desc)
+    # .page(params[:page]).per(25)
   end
 
   def show
      @user = User.find(params[:id])
   end
 
- def new
+  def new
     @user = User.new
   end
 
